253005eefde368904e8db17bf03451a7cd0cc131
sean <playoriginstory@gmail.com>
2025-11-14 23:34:43 +0100

    fix

--- app/components/query-provider.tsx
+++ app/components/query-provider.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter, Source_Code_Pro } from "next/font/google";
-import "./globals.css";
 import QueryProvider from "./query-provider"; // 👇 new client wrapper
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
